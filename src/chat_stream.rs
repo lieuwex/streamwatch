1695cedbd96a1c3e4eec478ea6014e3d34cabedb
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2021-04-13 14:32:44 +0200

    FileReader struct

--- src/chat_stream.rs
+++ src/chat_stream.rs
@@ -1,3 +1,4 @@
+use super::types::StreamInfo;
 use super::DB;
 
 use futures::{SinkExt, StreamExt};
@@ -9,7 +10,7 @@ use chrono::{DateTime, Duration, FixedOffset, Local, TimeZone, Utc};
 
 use tokio;
 use tokio::fs::File;
-use tokio::io::{AsyncBufReadExt, BufReader};
+use tokio::io::{AsyncBufReadExt, BufReader, Lines};
 
 use serde::{Deserialize, Serialize};
 
@@ -34,15 +35,91 @@ struct WebSocketResponse {
     res: Vec<Item>,
 }
 
-fn parse_line(line: &str) -> Result<(DateTime<FixedOffset>, &str), String> {
-    let splitted: Vec<_> = line.splitn(2, ' ').collect();
+struct FileReader {
+    orphan: Option<(DateTime<FixedOffset>, String)>,
+    prev_datetime: DateTime<FixedOffset>,
+    stream: StreamInfo,
+    lines: Lines<BufReader<ZstdDecoder<BufReader<File>>>>,
+}
 
-    let date = match DateTime::parse_from_rfc3339(splitted[0]) {
-        Err(e) => return Err(e.to_string()),
-        Ok(d) => d,
-    };
+impl FileReader {
+    async fn create_lines(stream: &StreamInfo) -> Lines<BufReader<ZstdDecoder<BufReader<File>>>> {
+        let f = File::open(stream.chat_file_path()).await.unwrap();
+        let reader = BufReader::new(ZstdDecoder::new(BufReader::new(f)));
+        reader.lines()
+    }
 
-    Ok((date, splitted[1]))
+    fn parse_line(line: &str) -> Result<(DateTime<FixedOffset>, &str), String> {
+        let splitted: Vec<_> = line.splitn(2, ' ').collect();
+
+        let date = match DateTime::parse_from_rfc3339(splitted[0]) {
+            Err(e) => return Err(e.to_string()),
+            Ok(d) => d,
+        };
+
+        Ok((date, splitted[1]))
+    }
+
+    async fn new(stream: StreamInfo) -> Self {
+        let local_ts = Local.timestamp(0, 0);
+        let tz_offset = local_ts.offset();
+
+        let lines = Self::create_lines(&stream).await;
+
+        Self {
+            orphan: None,
+            prev_datetime: DateTime::from_utc(stream.datetime() - Duration::hours(1), *tz_offset),
+            stream,
+            lines,
+        }
+    }
+
+    async fn get_between(&mut self, start: DateTime<Utc>, end: DateTime<Utc>) -> Vec<Item> {
+        // we are going back to the past, so we have to repon the file to seek to the file start.
+        if self.prev_datetime > start {
+            eprintln!(
+                "!!! seeking back prev_datetime={} start={}",
+                self.prev_datetime, start
+            );
+
+            self.lines = Self::create_lines(&self.stream).await;
+        }
+
+        let mut res = Vec::new();
+
+        if let Some((datetime, json)) = self.orphan.take() {
+            if start <= datetime {
+                self.prev_datetime = datetime;
+                res.push(Item {
+                    ts: datetime.timestamp_millis() as usize,
+                    content: json,
+                });
+            }
+        }
+
+        while let Some(line) = self.lines.next_line().await.unwrap() {
+            let (datetime, json) = Self::parse_line(&line).unwrap();
+
+            if datetime > end {
+                self.orphan = Some((datetime, json.to_string()));
+                break;
+            }
+
+            self.prev_datetime = datetime;
+
+            // loop until we reached the starting point
+            if datetime < start {
+                continue;
+            }
+
+            res.push(Item {
+                ts: datetime.timestamp_millis() as usize,
+                content: json.to_string(),
+            });
+        }
+
+        res
+    }
 }
 
 async fn websocket_handler(stream_id: u64, mut ws: WebSocket) {
@@ -58,20 +135,8 @@ async fn websocket_handler(stream_id: u64, mut ws: WebSocket) {
         return;
     }
 
-    let chat_path = stream.chat_file_path();
+    let mut file_reader = FileReader::new(stream).await;
 
-    // get the stream start date
-    let local_ts = Local.timestamp(0, 0);
-    let tz_offset = local_ts.offset();
-    let mut prev_datetime: DateTime<FixedOffset> =
-        DateTime::from_utc(stream.datetime() - Duration::hours(1), *tz_offset);
-
-    // file state
-    let f = File::open(chat_path.clone()).await.unwrap();
-    let mut reader = BufReader::new(ZstdDecoder::new(BufReader::new(f)));
-    let mut lines = reader.lines();
-
-    let mut orphan: Option<(DateTime<_>, String)> = None;
     while let Some(message) = ws.next().await {
         let (id, start, end) = {
             let message = match message {
@@ -91,51 +156,7 @@ async fn websocket_handler(stream_id: u64, mut ws: WebSocket) {
             (request.id, start, end)
         };
 
-        // we are going back to the past, so we have to repon the file to seek to the file start.
-        if prev_datetime > start {
-            eprintln!(
-                "!!! seeking back prev_datetime={} start={}",
-                prev_datetime, start
-            );
-
-            let f = File::open(chat_path.clone()).await.unwrap();
-            reader = BufReader::new(ZstdDecoder::new(BufReader::new(f)));
-
-            lines = reader.lines();
-        }
-
-        let mut res = Vec::new();
-
-        if let Some((datetime, json)) = orphan.take() {
-            if start <= datetime {
-                prev_datetime = datetime;
-                res.push(Item {
-                    ts: datetime.timestamp_millis() as usize,
-                    content: json,
-                });
-            }
-        }
-
-        while let Some(line) = lines.next_line().await.unwrap() {
-            let (datetime, json) = parse_line(&line).unwrap();
-
-            if datetime > end {
-                orphan = Some((datetime, json.to_string()));
-                break;
-            }
-
-            prev_datetime = datetime;
-
-            // loop until we reached the starting point
-            if datetime < start {
-                continue;
-            }
-
-            res.push(Item {
-                ts: datetime.timestamp_millis() as usize,
-                content: json.to_string(),
-            });
-        }
+        let res = file_reader.get_between(start, end).await;
 
         let s = serde_json::to_string(&WebSocketResponse { id, res }).unwrap();
         if ws.send(warp::ws::Message::text(s)).await.is_err() {
