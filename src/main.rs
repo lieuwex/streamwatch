25df4d97eff7d596e5c3c581e84040158b639399
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2021-04-21 17:58:07 +0200

    manual streams rescan

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ use crate::types::*;
 use tokio;
 use tokio::fs::{read_dir, remove_dir_all, remove_file};
 use tokio::sync;
-use tokio::time;
 use tokio_stream::wrappers::ReadDirStream;
 
 use regex::Regex;
@@ -42,7 +41,9 @@ pub static FILE_STEM_REGEX_DATE: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"^\d{4}-\d{2}-\d{2}").unwrap());
 
 pub static DB: OnceCell<Arc<Mutex<db::Database>>> = OnceCell::new();
+static SENDER: OnceCell<tokio::sync::mpsc::Sender<Job>> = OnceCell::new();
 pub static STREAMS_DIR: &'static str = "/streams/lekkerspelen";
+
 static PREVIEW_WORKERS: usize = 4;
 
 fn get_preview_path(stream_id: i64) -> PathBuf {
@@ -164,6 +165,7 @@ fn get_possible_persons() -> impl warp::Reply {
     warp::reply::json(&possible_persons)
 }
 
+#[derive(PartialEq, Eq)]
 enum ItemState {
     Unchanged,
     New,
@@ -267,304 +269,163 @@ fn parse_filename(path_buf: &PathBuf) -> Option<DateTime<Local>> {
     Some(Local.from_local_datetime(&naive_datetime).unwrap())
 }
 
-async fn file_watcher(db: Arc<Mutex<db::Database>>, sender: sync::mpsc::Sender<Job>) {
-    loop {
-        let file_name_states = {
-            let db_map: HashMap<String, u64> = {
-                let db = db.lock().unwrap();
-                db.get_streams()
-                    .into_iter()
-                    .map(|stream| (stream.file_name, stream.file_size))
-                    .collect()
-            };
-            let dir_map: HashMap<String, u64> = {
-                let dir = read_dir(STREAMS_DIR).await.unwrap();
-                ReadDirStream::new(dir)
-                    .then(|item| async {
-                        let item = item.unwrap();
-                        let file_name = item.file_name().to_str().unwrap().to_owned();
-                        let size = item.metadata().await.unwrap().len();
+async fn rescan_streams() -> Result<impl warp::Reply, warp::Rejection> {
+    let db = DB.get().unwrap();
+    let sender = SENDER.get().unwrap();
 
-                        (file_name, size)
-                    })
-                    .collect()
-                    .await
-            };
+    let file_name_states = {
+        let db_map: HashMap<String, u64> = {
+            let db = db.lock().unwrap();
+            db.get_streams()
+                .into_iter()
+                .map(|stream| (stream.file_name, stream.file_size))
+                .collect()
+        };
+        let dir_map: HashMap<String, u64> = {
+            let dir = read_dir(STREAMS_DIR).await.unwrap();
+            ReadDirStream::new(dir)
+                .then(|item| async {
+                    let item = item.unwrap();
+                    let file_name = item.file_name().to_str().unwrap().to_owned();
+                    let size = item.metadata().await.unwrap().len();
 
-            let mut m: HashMap<String, (u64, ItemState)> = HashMap::new();
-
-            for dir_file in &dir_map {
-                if !(dir_file.0.ends_with(".mp4")
-                    || dir_file.0.ends_with(".mkv")
-                    || dir_file.0.ends_with(".webm"))
-                {
-                    continue;
-                }
-
-                let state = match db_map.get(dir_file.0) {
-                    Some(db_file_size) => {
-                        if db_file_size == dir_file.1 {
-                            ItemState::Unchanged
-                        } else {
-                            ItemState::Modified
-                        }
-                    }
-                    None => ItemState::New,
-                };
-
-                m.insert(dir_file.0.to_string(), (*dir_file.1, state));
-            }
-
-            for db_file in db_map {
-                if dir_map.contains_key(&db_file.0) {
-                    continue;
-                }
-
-                m.insert(db_file.0, (db_file.1, ItemState::Removed));
-            }
-
-            m
+                    (file_name, size)
+                })
+                .collect()
+                .await
         };
 
-        for (file_name, (file_size, state)) in file_name_states {
-            let path = Path::new(STREAMS_DIR).join(file_name.clone());
+        let mut m: HashMap<String, (u64, ItemState)> = HashMap::new();
 
-            match state {
-                ItemState::Unchanged => {}
-                ItemState::New => {
-                    println!("got new item: {}", file_name);
+        for dir_file in &dir_map {
+            if !(dir_file.0.ends_with(".mp4")
+                || dir_file.0.ends_with(".mkv")
+                || dir_file.0.ends_with(".webm"))
+            {
+                continue;
+            }
 
-                    let timestamp = match parse_filename(&path) {
-                        Some(date) => date.timestamp(),
-                        None => {
-                            eprintln!("error parsing timestamp for {:?}", path);
-                            0
-                        }
-                    };
+            let state = match db_map.get(dir_file.0) {
+                Some(db_file_size) => {
+                    if db_file_size == dir_file.1 {
+                        ItemState::Unchanged
+                    } else {
+                        ItemState::Modified
+                    }
+                }
+                None => ItemState::New,
+            };
 
-                    let duration = match get_video_duration_in_secs(&path).await {
-                        Ok(d) => d,
-                        Err(_) => {
-                            eprintln!("error getting duration for: {:?}", path);
-                            continue;
-                        }
-                    };
+            m.insert(dir_file.0.to_string(), (*dir_file.1, state));
+        }
 
-                    let stream_id: i64 = {
-                        let db = db.lock().unwrap();
-                        db.conn.execute(
+        for db_file in db_map {
+            if dir_map.contains_key(&db_file.0) {
+                continue;
+            }
+
+            m.insert(db_file.0, (db_file.1, ItemState::Removed));
+        }
+
+        m
+    };
+
+    let mut all_unchanged = true;
+    for (file_name, (file_size, state)) in file_name_states {
+        let path = Path::new(STREAMS_DIR).join(file_name.clone());
+
+        match state {
+            ItemState::Unchanged => {}
+            ItemState::New => {
+                println!("got new item: {}", file_name);
+                all_unchanged = false;
+
+                let timestamp = match parse_filename(&path) {
+                    Some(date) => date.timestamp(),
+                    None => {
+                        eprintln!("error parsing timestamp for {:?}", path);
+                        0
+                    }
+                };
+
+                let duration = match get_video_duration_in_secs(&path).await {
+                    Ok(d) => d,
+                    Err(_) => {
+                        eprintln!("error getting duration for: {:?}", path);
+                        continue;
+                    }
+                };
+
+                let stream_id: i64 = {
+                    let db = db.lock().unwrap();
+                    db.conn.execute(
                             "INSERT INTO streams(filename, filesize, ts, duration) values(?1, ?2, ?3, ?4)",
                             params![file_name, file_size as i64, timestamp, duration as f64],
                         )
                         .unwrap();
-                        db.conn.last_insert_rowid()
-                    };
+                    db.conn.last_insert_rowid()
+                };
 
-                    sender
-                        .send(Job::Thumbnails {
-                            stream_id,
-                            path: path.clone(),
-                        })
-                        .await
-                        .unwrap();
-                    sender.send(Job::Preview { stream_id, path }).await.unwrap();
-                }
-                ItemState::Modified => {
-                    println!("got updated item: {}", file_name);
-
-                    let stream_id = {
-                        let db = db.lock().unwrap();
-
-                        let stream_id = db.get_stream_id_by_filename(&file_name).unwrap();
-
-                        // update filesize
-                        db.conn
-                            .execute(
-                                "UPDATE streams SET filesize = ?1 WHERE id = ?2",
-                                params![file_size as i64, stream_id],
-                            )
-                            .unwrap();
-
-                        stream_id
-                    };
-
-                    remove_thumbnails_and_preview(stream_id).await;
-
-                    sender
-                        .send(Job::Thumbnails {
-                            stream_id,
-                            path: path.clone(),
-                        })
-                        .await
-                        .unwrap();
-                    sender.send(Job::Preview { stream_id, path }).await.unwrap();
-                }
-                ItemState::Removed => {
-                    println!("got removed item: {}", file_name);
-
-                    let stream_id = db
-                        .lock()
-                        .unwrap()
-                        .get_stream_id_by_filename(&file_name)
-                        .unwrap();
-                    remove_thumbnails_and_preview(stream_id).await;
-                    db.lock().unwrap().remove_stream(stream_id);
-                }
+                sender
+                    .send(Job::Thumbnails {
+                        stream_id,
+                        path: path.clone(),
+                    })
+                    .await
+                    .unwrap();
+                sender.send(Job::Preview { stream_id, path }).await.unwrap();
             }
-        }
+            ItemState::Modified => {
+                println!("got updated item: {}", file_name);
+                all_unchanged = false;
 
-        time::sleep(time::Duration::from_secs(60)).await;
-    }
-}
+                let stream_id = {
+                    let db = db.lock().unwrap();
 
-/*
-async fn file_watcher(db: Arc<Mutex<db::Database>>, sender: sync::mpsc::Sender<Job>) {
-    let mut map: HashMap<_, _> = {
-        let db = db.lock().unwrap();
-        db.get_streams()
-            .into_iter()
-            .map(|stream| (stream.file_name, stream.file_size))
-            .collect()
-    };
+                    let stream_id = db.get_stream_id_by_filename(&file_name).unwrap();
 
-    loop {
-        let mut dir = read_dir(STREAMS_DIR).await.unwrap();
-
-        while let Some(item) = dir.next_entry().await.unwrap() {
-            let file_name = item.file_name();
-            let size = item.metadata().await.unwrap().len();
-
-            let entry = map.entry(file_name.to_string_lossy().to_string());
-            let state = match entry {
-                Entry::Vacant(item) => {
-                    item.insert(size);
-                    ItemState::New
-                }
-                Entry::Occupied(mut item) => {
-                    let old_size = item.get();
-                    let modified = *old_size != size;
-                    if modified {
-                        item.insert(size);
-                        ItemState::Modified
-                    } else {
-                        ItemState::Unchanged
-                    }
-                }
-            };
-
-            let path = Path::new(STREAMS_DIR).join(file_name.clone());
-
-            // HACK
-            match path.extension() {
-                None => continue,
-                Some(e) => {
-                    if !(e == "mp4" || e == "mkv" || e == "webm") {
-                        continue;
-                    }
-                }
-            }
-
-            // TODO: also handle remove items
-
-            match state {
-                ItemState::Unchanged => {}
-                ItemState::New => {
-                    println!("got new item: {}", file_name.to_string_lossy());
-
-                    let timestamp = {
-                        match NaiveDateTime::parse_from_str(
-                            path.file_stem().unwrap().to_str().unwrap(),
-                            "%Y-%m-%d %H:%M:%S",
-                        ) {
-                            Ok(date) => date.timestamp(),
-                            Err(_) => {
-                                eprintln!("error parsing timestamp for: {:?}", path);
-                                0
-                            }
-                        }
-                    };
-                    let duration = match get_video_duration_in_secs(&path).await {
-                        Ok(d) => d,
-                        Err(_) => {
-                            eprintln!("error getting duration for: {:?}", path);
-                            continue;
-                        }
-                    };
-
-                    let stream_id: i64 = {
-                        let db = db.lock().unwrap();
-                        db.conn.execute(
-                            "INSERT INTO streams(filename, filesize, ts, duration) values(?1, ?2, ?3, ?4)",
-                            params![file_name.to_str().unwrap(), size as i64, timestamp, duration as f64],
+                    // update filesize
+                    db.conn
+                        .execute(
+                            "UPDATE streams SET filesize = ?1 WHERE id = ?2",
+                            params![file_size as i64, stream_id],
                         )
                         .unwrap();
-                        db.conn.last_insert_rowid()
-                    };
 
-                    sender
-                        .send(Job::Thumbnails(stream_id, path.clone()))
-                        .await
-                        .unwrap();
-                    sender.send(Job::Preview(stream_id, path)).await.unwrap();
-                }
-                ItemState::Modified => {
-                    println!("got updated item: {}", file_name.to_string_lossy());
+                    stream_id
+                };
 
-                    let stream_id = {
-                        let db = db.lock().unwrap();
+                remove_thumbnails_and_preview(stream_id).await;
 
-                        // get stream_id by filename
-                        let stream_id: i64 = db
-                            .conn
-                            .query_row(
-                                "SELECT id from streams where filename = ?1",
-                                params![file_name.to_str().unwrap()],
-                                |row| row.get(0),
-                            )
-                            .unwrap();
+                sender
+                    .send(Job::Thumbnails {
+                        stream_id,
+                        path: path.clone(),
+                    })
+                    .await
+                    .unwrap();
+                sender.send(Job::Preview { stream_id, path }).await.unwrap();
+            }
+            ItemState::Removed => {
+                println!("got removed item: {}", file_name);
 
-                        // update filesize
-                        db.conn
-                            .execute(
-                                "UPDATE streams SET filesize = ?1 WHERE id = ?2",
-                                params![size as i64, stream_id],
-                            )
-                            .unwrap();
-
-                        // remove preview
-                        db.conn
-                            .execute(
-                                "DELETE FROM stream_previews WHERE stream_id = ?1",
-                                params![stream_id],
-                            )
-                            .unwrap();
-                        // remove thumbnails
-                        db.conn
-                            .execute(
-                                "DELETE FROM stream_thumbnails WHERE stream_id = ?1",
-                                params![stream_id],
-                            )
-                            .unwrap();
-
-                        stream_id
-                    };
-
-                    log_err!(remove_file(get_preview_path(stream_id)).await);
-                    log_err!(remove_dir_all(get_thumbnails_path(stream_id)).await);
-
-                    sender
-                        .send(Job::Thumbnails(stream_id, path.clone()))
-                        .await
-                        .unwrap();
-                    sender.send(Job::Preview(stream_id, path)).await.unwrap();
-                }
+                let stream_id = db
+                    .lock()
+                    .unwrap()
+                    .get_stream_id_by_filename(&file_name)
+                    .unwrap();
+                remove_thumbnails_and_preview(stream_id).await;
+                db.lock().unwrap().remove_stream(stream_id);
             }
         }
-
-        time::sleep(time::Duration::from_secs(60)).await;
     }
+
+    if all_unchanged {
+        println!("no new/modified items found");
+    }
+
+    Ok(warp::reply())
 }
-*/
 
 async fn job_watcher(receiver: Arc<sync::Mutex<sync::mpsc::Receiver<Job>>>) {
     loop {
@@ -592,11 +453,11 @@ async fn main() -> io::Result<()> {
     }
 
     let (sender, receiver) = sync::mpsc::channel(1);
+    match SENDER.set(sender) {
+        Ok(_) => {}
+        Err(_) => panic!("oncecell already full"),
+    }
     let receiver = Arc::new(sync::Mutex::new(receiver));
-
-    tokio::spawn(async move {
-        file_watcher(db, sender).await;
-    });
 
     for _ in 0..PREVIEW_WORKERS {
         let receiver_cloned = receiver.clone();
@@ -612,7 +473,10 @@ async fn main() -> io::Result<()> {
     warp::serve({
         let cors = warp::cors().allow_any_origin();
 
-        let compressed = (warp::path("streams").map(streams))
+        let compressed = (warp::get().and(warp::path!("streams")).map(streams))
+            .or(warp::patch()
+                .and(warp::path!("streams"))
+                .and_then(rescan_streams))
             .or(warp::get()
                 .and(warp::path!("persons"))
                 .map(get_possible_persons))
