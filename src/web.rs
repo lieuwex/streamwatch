lieuwex/streamwatch

commit 22f8847aa49aa330a3880dbeebb22effd2619ba9

use transaction in handle_new_stream
Sometimes this function fails when a new stream is added which uses
games in the datapoints which are found in the database with the field
name set to the name in the datapoints, but where the twitch_name is
unset.
We try adding a new game with name=twitch_name=dp.game_name, which fails
the UNIQUEness check for name. Totally expected.

However, the problem now comes that since we weren't executing the
database operations in handle_new_stream in a transaction, we leave the
database in a half broken state.

This commit makes it such that all operations done in handle_new_stream
are using the same transaction, makeing it able to rollback everything
on any error.
This commit also requires some changes in db.rs, and files that call
functions in db.rs.

src/web.rs
@@ -8,6 +8,9 @@ use crate::{check, DB, STREAMS_DIR};
 use std::collections::HashMap;
 use std::net::IpAddr;
 use std::str::FromStr;
+use std::sync::Arc;
+
+use tokio::sync::Mutex;
 
 use warp::http::StatusCode;
 use warp::{Filter, Reply};
@@ -48,7 +51,8 @@ async fn replace_games(
     items: Vec<GameItem>,
 ) -> Result<warp::reply::Response, warp::Rejection> {
     let db = DB.get().unwrap();
-    check!(db.replace_games(stream_id, items).await);
+    let tx = Arc::new(Mutex::new(db.pool.begin().await.unwrap()));
+    check!(db.replace_games(tx, stream_id, items).await);
 
     Ok(warp::reply().into_response())
 }
@@ -176,13 +180,21 @@ async fn add_possible_game(
 ) -> Result<warp::reply::Json, warp::Rejection> {
     let db = DB.get().unwrap();
 
-    let name = check!(info.get("name").ok_or_else(|| anyhow!("name required")));
-    let twitch_name = info.get("twitchName");
-    let platform = info.get("platform");
-    let game_info = check!(
-        db.insert_possible_game(name.to_owned(), twitch_name.cloned(), platform.cloned())
+    let game_info = {
+        let name = check!(info.get("name").ok_or_else(|| anyhow!("name required")));
+        let twitch_name = info.get("twitchName");
+        let platform = info.get("platform");
+
+        check!(
+            db.insert_possible_game(
+                &db.pool,
+                name.to_owned(),
+                twitch_name.cloned(),
+                platform.cloned()
+            )
             .await
-    );
+        )
+    };
 
     Ok(warp::reply::json(&game_info))
 }

src/db.rs
@@ -5,16 +5,19 @@ use crate::types::{
 };
 
 use std::collections::HashMap;
+use std::ops::DerefMut;
+use std::sync::Arc;
 use std::time::Instant;
 
 use sqlx::sqlite::{SqlitePool, SqliteRow};
-use sqlx::Row;
+use sqlx::{Row, Transaction};
 
 use anyhow::Result;
 
 use chrono::{DateTime, Utc};
 
 use futures::TryStreamExt;
+use tokio::sync::Mutex;
 
 pub struct Database {
     pub pool: sqlx::SqlitePool,
@@ -183,19 +186,23 @@ impl Database {
             .await?;
         Ok(res)
     }
-    pub async fn insert_possible_game(
+    pub async fn insert_possible_game<'c, E>(
         &self,
+        executor: E,
         name: String,
         twitch_name: Option<String>,
         platform: Option<String>,
-    ) -> Result<GameInfo> {
+    ) -> Result<GameInfo>
+    where
+        E: sqlx::Executor<'c, Database = sqlx::sqlite::Sqlite>,
+    {
         let res = sqlx::query!(
             "INSERT INTO GAMES(name, platform, twitch_name) VALUES(?1, ?2, ?3)",
             name,
             platform,
             twitch_name
         )
-        .execute(&self.pool)
+        .execute(executor)
         .await?;
         Ok(GameInfo {
             name,
@@ -205,14 +212,17 @@ impl Database {
         })
     }
 
-    pub async fn replace_games<I>(&self, stream_id: i64, items: I) -> Result<()>
+    pub async fn replace_games<'c, I>(
+        &self,
+        tx: Arc<Mutex<Transaction<'c, sqlx::Sqlite>>>,
+        stream_id: i64,
+        items: I,
+    ) -> Result<()>
     where
         I: IntoIterator<Item = GameItem>,
     {
-        let mut tx = self.pool.begin().await?;
-
         sqlx::query!("DELETE FROM game_features WHERE stream_id = ?1", stream_id)
-            .execute(&mut tx)
+            .execute(tx.lock().await.deref_mut())
             .await?;
 
         for item in items {
@@ -222,11 +232,10 @@ impl Database {
                 item.id,
                 item.start_time
             )
-            .execute(&mut tx)
+            .execute(tx.lock().await.deref_mut())
             .await?;
         }
 
-        tx.commit().await?;
         Ok(())
     }
 

src/scan.rs
@@ -4,10 +4,12 @@ use crate::{create_preview::get_video_duration_in_secs, types::GameInfo};
 use crate::{DB, STREAMS_DIR};
 
 use std::collections::HashMap;
+use std::ops::DerefMut;
 use std::path::Path;
+use std::sync::Arc;
 
-use anyhow::Result;
 use tokio::fs::{read_dir, remove_dir_all, remove_file};
+use tokio::sync::Mutex;
 use tokio_stream::wrappers::ReadDirStream;
 
 use futures::stream::iter;
@@ -19,7 +21,7 @@ use chrono::{DateTime, Local, NaiveDate, NaiveDateTime, TimeZone};
 
 use regex::Regex;
 
-use anyhow::bail;
+use anyhow::{bail, Result};
 
 macro_rules! log_err {
     ($item:expr) => {
@@ -97,6 +99,8 @@ async fn handle_new_stream(
         .await?
         .unwrap_or((vec![], vec![]));
 
+    let tx = Arc::new(Mutex::new(db.pool.begin().await?));
+
     let stream_id: i64 = {
         let duration = f64::from(duration);
         let has_chat = file_name.has_chat().await?;
@@ -115,7 +119,7 @@ async fn handle_new_stream(
             datapoints_json,
             jumpcuts_json,
         )
-        .execute(&db.pool)
+        .execute(tx.lock().await.deref_mut())
         .await?
         .last_insert_rowid()
     };
@@ -123,6 +127,7 @@ async fn handle_new_stream(
     struct FoldState<'a> {
         games: Vec<GameFeature>,
         possible_games: &'a mut Vec<GameInfo>,
+        tx: Arc<Mutex<sqlx::Transaction<'a, sqlx::Sqlite>>>,
     }
 
     let games = iter(datapoints)
@@ -131,6 +136,7 @@ async fn handle_new_stream(
             FoldState {
                 games: vec![],
                 possible_games,
+                tx: tx.clone(),
             },
             |mut state: FoldState<'_>, datapoint| async move {
                 let last_item_same_game = state
@@ -154,6 +160,7 @@ async fn handle_new_stream(
                     None => {
                         let game = db
                             .insert_possible_game(
+                                state.tx.clone().lock_owned().await.deref_mut(),
                                 datapoint.game.clone(),
                                 Some(datapoint.game),
                                 None,
@@ -179,7 +186,12 @@ async fn handle_new_stream(
             id: g.info.id,
             start_time: g.start_time,
         });
-    db.replace_games(stream_id, games).await?;
+    db.replace_games(tx.clone(), stream_id, games).await?;
+
+    match Arc::try_unwrap(tx) {
+        Ok(mutex) => mutex.into_inner().commit().await?,
+        Err(arc) => bail!("arc is owned more than once: {}", Arc::strong_count(&arc)),
+    }
 
     sender.send(Job::Thumbnails {
         stream_id,

commit 95a13f080cef25c300a6883c2c2f3cb552504c34

commit transaction in web::replace_games
This was broken in 22f8847aa49aa330a3880dbeebb22effd2619ba9.

src/web.rs
@@ -3,7 +3,7 @@ use crate::scan::scan_streams;
 use crate::types::{ConversionProgress, CreateClipRequest, GameItem, StreamJson};
 use crate::util::AnyhowError;
 use crate::watchparty::{get_watch_parties, watch_party_ws};
-use crate::{check, DB, STREAMS_DIR};
+use crate::{arc_mutex_unwrap, check, DB, STREAMS_DIR};
 
 use std::collections::HashMap;
 use std::net::IpAddr;
@@ -51,8 +51,10 @@ async fn replace_games(
     items: Vec<GameItem>,
 ) -> Result<warp::reply::Response, warp::Rejection> {
     let db = DB.get().unwrap();
+
     let tx = Arc::new(Mutex::new(db.pool.begin().await.unwrap()));
-    check!(db.replace_games(tx, stream_id, items).await);
+    check!(db.replace_games(tx.clone(), stream_id, items).await);
+    check!(arc_mutex_unwrap!(tx)).commit().await.unwrap();
 
     Ok(warp::reply().into_response())
 }