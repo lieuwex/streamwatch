f6b5eb686b41e29c62cb48eec52bf882f5714a36
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2024-06-30 17:56:38 +0200

    update dependencies

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/7cd0de9b17d00965770d9fc2825a34a7f13fd310.tar.gz") {});
+with (import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/release-24.05.tar.gz") {});
 
 pkgs.mkShell {
 	buildInputs = [
-		rust-bin.nightly."2023-08-20".complete
+		rust-bin.nightly."2024-06-29".complete
 		cargo-outdated
 
 		openssl
